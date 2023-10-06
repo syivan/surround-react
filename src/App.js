cb8e65249af66fd65045da5aed029312667cd9fa
syivan <ivansyrups@gmail.com>
2023-10-05 23:59:35 -0700

    grid structure implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootswatch/dist/lux/bootstrap.min.css";
 import NavHeader from "./components/NavHeader.js";
+import { Container, Row, Col } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
       <NavHeader />
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
     </>
   );
 };
