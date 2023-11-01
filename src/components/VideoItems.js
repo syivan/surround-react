16845592d44f0f88317534b7fe38f91cd18ab160
Ivan Sy <ivansyrups@gmail.com>
2023-11-01 00:18:10 -0700

    constructed finished grid (row/col) thumbnails

--- src/components/VideoItems.js
+++ src/components/VideoItems.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack, Card } from 'react-bootstrap';
+import { Row, Card } from 'react-bootstrap';
 
 const VideoItems = ({ videos }) => {
 
@@ -7,17 +7,21 @@ const VideoItems = ({ videos }) => {
 
     const videoMap = videos.map((item, index) => (
         <Card key={index} style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={item.snippet.thumbnails.high["url"]}></Card.Img>
             <Card.Body>
                 <Card.Title>{item.snippet.title}</Card.Title>
+                <Card.Text>
+                    {item.snippet.description}
+                </Card.Text>
             </Card.Body>
         </Card >
     )
     )
 
     return (
-        <Stack className="d-flex flex-wrap justify-content-start" direction="horizontal" gap={3}>
+        <Row className="d-flex flex-wrap justify-content-start gap-4" direction="horizontal" >
             {videoMap}
-        </Stack>
+        </Row>
     )
 }
 
