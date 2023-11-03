92dd49b164a7db53a1f46a4331fc2b421f3376a3
syivan <ivansyrups@gmail.com>
2023-11-02 22:50:41 -0700

    convert stack to row/ design implementation

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import "../style.css";
 
 const VideoCard = ({ item }) => {
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.snippet.thumbnails.high["url"]}></Card.Img>
-            <Card.Body>
-                <Card.Title>{item.snippet.title}</Card.Title>
-                <Card.Text>
-                    {item.snippet.description}
-                </Card.Text>
-            </Card.Body>
-        </Card >
-    )
-}
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        src={item.snippet.thumbnails.high["url"]}
+        className="card-img"
+      ></Card.Img>
+      <Card.Body>
+        <Card.Title>{item.snippet.title}</Card.Title>
+        <Card.Text>{item.snippet.description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
--- src/components/VideoItems.js
+++ src/components/VideoItems.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Row, Container } from 'react-bootstrap';
-import VideoCard from './VideoCard';
+import React from "react";
+import { Row, Container } from "react-bootstrap";
+import VideoCard from "./VideoCard";
 
 const VideoItems = ({ videos }) => {
+  console.log(videos);
 
-    console.log(videos);
+  const videoMap = videos.map((item, index) => (
+    <Container key={index}>
+      <VideoCard item={item} />
+    </Container>
+  ));
 
-    const videoMap = videos.map((item, index) => (
-        <Container key={index}>
-            <VideoCard item={item} />
-        </Container>
-    )
-    )
+  return (
+    // <Row
+    //   className="d-inline-flex flex-wrap justify-content-start gap-2"
+    //   direction="horizontal"
+    // >
+    //   {videoMap}
+    // </Row>
+    <Row md={3} lg={4} xs={2} className="">
+      {videoMap}
+    </Row>
+  );
+};
 
-    return (
-        <Row className="d-flex flex-wrap justify-content-start gap-4" direction="horizontal" >
-            {videoMap}
-        </Row>
-    )
-}
-
-export default VideoItems
+export default VideoItems;
