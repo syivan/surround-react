8e646b09c97572cb188489be9b3da458d7075733
syivan <ivansyrups@gmail.com>
2024-01-05 01:47:50 -0800

    bug fixes/ about section complete

--- src/components/About.js
+++ src/components/About.js
@@ -27,9 +27,73 @@ const About = () => {
 
       <p>
         Credits to the following people who granted me licensed use of their
-        images and recorded audio:
+        icons and recorded audio:
       </p>
-      <ul></ul>
+      <section className="credits">
+        <details className="image-credits">
+          <summary>Images</summary>
+          <ul>
+            <li>
+              <a href="https://www.iconfinder.com/ladalle">Ladalle CS</a>
+            </li>
+            <li>
+              <a href="https://www.iconfinder.com/akmal-mahdi">Akmal Mahdi</a>
+            </li>
+            <li>
+              <a href="https://www.iconfinder.com/Oppossume">Alex Timashenka</a>
+            </li>
+            <li>
+              <a href="https://www.iconfinder.com/Rengised">
+                Alexandr Martinov
+              </a>
+            </li>
+            <li>
+              <a href="https://www.iconfinder.com/hk12215">Olha Filipenko</a>
+            </li>
+          </ul>
+        </details>
+        <details className="audio-credits">
+          <summary>Audio Recordings</summary>
+          <ul>
+            <li>
+              <a href="https://pixabay.com/sound-effects/birds-19624/">
+                Pixabay
+              </a>{" "}
+              - Birds
+            </li>
+            <li>
+              <a href="https://freesound.org/people/Matias44/sounds/173920/">
+                Matias44
+              </a>{" "}
+              - Murmullo restaurante | Murmur restaurant
+            </li>
+            <li>
+              <a href="https://freesound.org/people/inchadney/sounds/83986/">
+                Inchadney
+              </a>{" "}
+              - fireplace
+            </li>
+            <li>
+              <a href="https://freesound.org/people/gluckose/sounds/333987/">
+                gluckose
+              </a>{" "}
+              - stream2
+            </li>
+            <li>
+              <a href="https://freesound.org/people/gepetto3548/sounds/689986/">
+                gepetto3548
+              </a>{" "}
+              - Excellent thunder & rain 25 Sept '21
+            </li>
+            <li>
+              <a href="https://pixabay.com/sound-effects/soft-rain-ambient-111154/">
+                SoundsForYou
+              </a>{" "}
+              - Soft Rain Ambient
+            </li>
+          </ul>
+        </details>
+      </section>
     </React.Fragment>
   );
 };
